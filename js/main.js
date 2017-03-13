87f53c5a4ce2f79c9683c51ade828dbad54a7e0e
Codija <pvlvcdavid@gmail.com>
2017-03-13 20:09:11 +0100

    Created cafe project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(44.843456, 20.407123),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 16
+    };
+    var venueMap;
+    venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function loadScript() {
+    var script = document.createElement('script');
+    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
