555ef0b9051d48f1bef6112c5b4663b1f070cc95
Codija <pvlvcdavid@gmail.com>
2017-03-13 21:56:53 +0100

    Fixed map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
-function init() {
-    var mapOptions = {
-        center: new google.maps.LatLng(44.843456, 20.407123),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 16
-    };
-    var venueMap;
-    venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 44.843456, lng: 20.407123},
+  zoom: 16
+});
 }
-
-function loadScript() {
-    var script = document.createElement('script');
-    script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyBlVnAXqLYftv-wDYkBXsNkuyEjwIcwKoo&callback=init';
-    document.body.appendChild(script);
-}
-
-window.onload = loadScript;
