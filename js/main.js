6e9f46c4b6dfc1ef3ea597f18bb492a9ba1f6cc1
Codija <pvlvcdavid@gmail.com>
2017-03-15 12:23:03 +0100

    Added scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 var map;
 function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 44.843456, lng: 20.407123},
-  zoom: 16
-});
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 44.843456, lng: 20.407123},
+      zoom: 16
+    });
 }
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.scroll-to-top').fadeIn();
+    } else {
+        $('.scroll-to-top').fadeOut();
+    }});
+  $('.scroll-to-top').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop : 0}, 800);
+});
